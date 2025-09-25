abff40a26434b2fa672b9c5c6458baaca1481c75
KIMYESEUL <kimesil1008@ajou.ac.kr>
2025-09-25 18:17:23 +0900

    250925

--- test1/src/main/java/com/example/test1/mapper/BoardMapper.java
+++ test1/src/main/java/com/example/test1/mapper/BoardMapper.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.test1.model.Board;
-import com.example.test1.model.Student;
 
 @Mapper
 public interface BoardMapper {
